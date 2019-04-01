18d577f0454aeb4d7e1838e04fd2737af49b84a3
段少珍 <duanshaozhen@xiaomi.com>
2019-04-01 20:05:19 +0800

    update

---
+++ controllers/FormatController.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+	"github.com/ziyoubiancheng/xcms/consts"
+	"github.com/ziyoubiancheng/xcms/models"
+)
+
+type FormatController struct {
+	BaseController
+}
+
+func (c *FormatController) Index() {
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["footerjs"] = "format/footerjs.html"
+	c.setTpl()
+}
